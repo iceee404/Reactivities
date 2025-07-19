6d2f94d475556df873244ad76991d8943da8345b
iceee404 <67895852+iceee404@users.noreply.github.com>
2025-07-19 19:24:28 +0800

    finished frontend walking skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>{
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+//添加cors配置
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(x=>x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000","https://localhost:3000"));
 app.MapControllers();
 
 //Configure Seed Data
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { ListItemText, List, ListItem, Typography } from "@mui/material";
 function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<Activity[]>("https://localhost:5001/api/activities")
+      .then((res) => {
+        setActivities(res.data);
+      });
+  }, []);
+
   return (
     <>
-      <h1>Activities</h1>
-      <p>This is a test</p>
+      <Typography variant="h2">Activities</Typography>
+      <List>
+        {activities.map((item) => (
+          <ListItem key={item.id}>
+            <ListItemText>{item.title}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 }
